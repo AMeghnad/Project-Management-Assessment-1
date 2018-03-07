296437e861c966890107f0aa4bb071f19be35bc3
AMeghnad <anshumanmeghnad@gmail.com>
2018-03-07 11:59:43 +1100

    Mechanic changes and code plan redo

---
+++ Assets/Scripts/GrappleHook.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrjMgmtFPS
+{
+    public class GrappleHook : MonoBehaviour
+    {
+        public Transform player, hook;
+        public bool isGrounded;
+        public float grappleSpeed = 10f;
+        public RaycastHit hookHit;
+        public LayerMask Building;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (isGrounded)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    if (Physics.Raycast(player.position, Vector3.forward, 1000f, Building))
+                    {
+
+                        transform.position = Vector3.Lerp(player.position, hook.position, grappleSpeed);
+                    }
+                }
+            }
+
+            if (!isGrounded)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+
+                }
+            }
+        }
+    }
+}
